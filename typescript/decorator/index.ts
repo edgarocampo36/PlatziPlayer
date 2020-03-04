03c4815649e7925f74143d88383b10ed7df9ea76
edgarocampo36 <edgarocampo36@gmail.com>
2020-03-04 20:37:45 -0300

    aplicacion de patrones observer, decoratory singleton

---
+++ typescript/decorator/index.ts
@@ -0,0 +1,56 @@
+class Field {
+    errors: string[]
+    input: HTMLInputElement
+
+    constructor(input: HTMLInputElement) {
+        this.input = input
+        this.errors = []
+    
+
+        let errorMessage = document.createElement('p')
+        errorMessage.className = 'text-danger'
+        this.input.parentNode.insertBefore(errorMessage, this.input.nextSibling)
+
+        this.input.addEventListener('input', () => {
+            this.errors = []
+            this.validate()
+            errorMessage.innerText = this.errors[0] || ''
+        })
+    }
+
+    validate() {
+
+    }
+}
+
+function RequiredFielDecorator(field: Field): Field {
+    let validate = field.validate
+
+    field.validate = function() {
+        validate()
+        let value = field.input.value
+        if (!value) {
+           field.errors.push("Requerido") 
+        }
+    }
+
+    return field
+}
+
+function EmailFielDecorator(field: Field): Field {
+    let validate = field.validate
+
+    field.validate = function() {
+        validate()
+        let value = field.input.value
+        if (value.indexOf("@") === -1) {
+           field.errors.push("Debe ser un email") 
+        }
+    }
+
+    return field
+}
+
+let field = new Field(document.querySelector('#email'))
+field = RequiredFielDecorator(field)
+field = EmailFielDecorator(field)
