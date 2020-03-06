58dc1bdcb0e52c2d8fdda9f07f0f89fc0586b729
Edgar Ocampo <edgarocampo36@gmail.com>
2020-03-06 16:06:33 -0300

    Se agrega plungin de Ads

--- assets/index.ts
+++ assets/index.ts
@@ -1,6 +1,7 @@
 import MediaPlayer from './MediaPlayer'
 import AutoPlay from './plugins/AutoPlay'
 import AutoPause from './plugins/AutoPause'
+import Ads from './plugins/Ads'
 
 const video = document.querySelector("video")
 const playButton: HTMLElement = document.querySelector("#playButton")
@@ -9,7 +10,7 @@ const muteButton: HTMLElement = document.querySelector("#muteButton")
 
 const player = new MediaPlayer({ 
     el: video, 
-    plugins: [new AutoPlay(), new AutoPause()] ,
+    plugins: [new AutoPlay(), new AutoPause(), new Ads()] ,
 })
 
 playButton.onclick = () => player.togglePlay()
